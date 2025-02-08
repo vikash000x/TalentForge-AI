63da0a1102eadf5476358c083492e3871cad1a7f
vikashsinha2603 <vikashsinha045@gmail.com>
2025-02-09 01:48:45 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,75 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AI Career Coach",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-muted/50 py-12">
+      <div className="container mx-auto px-4 text-center text-gray-200">
+        <p className="text-lg font-semibold">Welcome to Vikash Codex Clan 💗</p>
+        <div className="flex justify-center gap-6 mt-4">
+          <a
+            href="https://github.com/vikash000x"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-200 hover:text-gray-400 transition duration-300"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/vikash-sinha-215000259/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-200 hover:text-gray-400 transition duration-300"
+          >
+            <FaLinkedin size={24} />
+          </a>
+          <a
+            href="https://x.com/vikash_sinha_"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-200 hover:text-gray-400 transition duration-300"
+          >
+            <FaTwitter size={24} />
+          </a>
+        </div>
+      </div>
+    </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ data/testimonial.js
@@ -0,0 +1,28 @@
+export const testimonial = [
+  {
+    quote:
+      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+    author: "Ayush Raj",
+    image: "https://drive.google.com/uc?export=view&id=1jHRgjD2XL8Po1urMpY44A5-tuDQGjvji",
+    role: "Software Engineer",
+    company: "paypal",
+  },
+  {
+    quote:
+      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+    author: "Amir Hussain",
+    image: "https://drive.google.com/uc?export=view&id=1u59IdI4IwwZE-jAZ6d-Ly91tZjrQfg3l",
+    role: "web architecture",
+    company: "flipkart",
+  },
+  {
+    quote:
+      "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
+    author: "Raunak raj",
+    image: "https://drive.google.com/uc?export=view&id=1bdPhvBLFUwdLJKi11-sfQ0MrhoS790bM",
+    role: "Data Analytics",
+    company: "wallmart",
+  },
+];
+
+
